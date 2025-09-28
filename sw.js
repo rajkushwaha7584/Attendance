85e16b4dfd26d35f25addb93935dd5bfbcc5ec25
rajkushwaha7584 <rj8517884737@gmail.com>
2025-09-29 02:08:20 +0530

    new

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = "attendance-app-v1";
+const filesToCache = [
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png",
+  "https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
